ebdbaaf532cca31e047c4a36ba5b91150c6089b1
aryanSmalhotra <malhotra.singh.aryan@gmail.com>
2021-12-16 17:30:44 +0530

    Game Dev

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour {
      // Start is called before the first frame update
     void Start()
     {
-       // transform.position  = new Vector3(0,0,0);
+        transform.position  = new Vector3(0,0,0);
         
     }
 
@@ -23,6 +23,26 @@ public class Player : MonoBehaviour {
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right*horizontalInput*_speed*Time.deltaTime);
         transform.Translate(Vector3.up*verticalInput*_speed*Time.deltaTime);
+        if(transform.position.y >= 3.8f){
+            transform.position = new Vector3(transform.position.x,3.8f,0);
+        }
+        else if (transform.position.y <= -3.8f){
+            transform.position = new Vector3(transform.position.x,-3.8f,0);
+
+        }
+        
+        
+        
+        if(transform.position.x > 11.70208f)
+        {
+            transform.position = new Vector3(-11.70208f,transform.position.y,0);
+
+        }
+        else if (transform.position.x < -11.70208f){
+            transform.position = new Vector3(11.70208f,transform.position.y,0);
+
+        }
+
         
     }
     
