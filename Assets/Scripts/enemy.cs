40e10d5ad0fb5e62bddfc2a0061acc4250f31445
aryanSmalhotra <malhotra.singh.aryan@gmail.com>
2021-12-19 18:43:23 +0530

    code

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 4.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,12 @@ public class enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.down* _speed *Time.deltaTime);
+        if(transform.position.y < -11.6f){
+            transform.position = new Vector3(Random.Range(-10,10f),11,0);
+        }
+
         
     }
+
 }
